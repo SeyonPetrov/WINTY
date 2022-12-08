8dc9a459da10b6d06faa23574e949332879e9363
SAYMON <urta.1732@mail.ru>
2022-12-09 00:58:40 +0500

    dfghj

--- NIX.py
+++ NIX.py
@@ -1,46 +1,47 @@
 import pygame
 
-WH = pygame.Color('white')
-BK = pygame.Color('black')
-a, n = 0, 0
-try:  #
-    a, n = list(map(lambda x: int(x), input().split()))
-except ValueError:
-    print('Неправильный формат ввода')
-
-
-def draw_chess(screen):
-    s = screen
-    color = ''
-    t = a // n  #
-
-    if n % 2 == 0:  #
-        color = WH
-    else:
-        color = BK
-
-    for i in range(n):  #
-        for j in range(n):
-            print(color)
-            rect = (i * t, j * t, t, t)
-            pygame.draw.rect(s, color, rect)
-            if color != WH:
-                color = WH
-            else:
-                color = BK
-        if n % 2 == 0:
-            if color == WH:
-                color = BK
-            else:
-                color = WH
-
 
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = a, a
+    size = width, height = 700, 400
     screen = pygame.display.set_mode(size)
-    draw_chess(screen)
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+    for_cir = []
+    running = True
+    GO = False
+    pos = (0, 0)
+    clock = pygame.time.Clock()
+    speed_x = 300 * clock.tick() // 1000 * -1
+    speed_y = 300 * clock.tick() // 1000 * -1
+    while running:
+        for event in pygame.event.get():
+            scrn = pygame.Surface(screen.get_size())
+            if event.type == pygame.QUIT:
+                running = False
+            if pygame.mouse.get_pressed() == (1, 0, 0):
+                GO = True
+                pos = pygame.mouse.get_pos()
+                for_cir.append(pos)
+        print(pos)
+        if GO:
+
+            for r in range(len(for_cir)):
+                x = for_cir[r][0]
+                y = for_cir[r][1]
+
+                if (x - 10) - speed_x <= 0:
+                    speed_x = speed_x * -1
+                elif (x - 10) + speed_x >= width:
+                    speed_x = speed_x * -1
+                elif (y - 10) - speed_y <= 0:
+                    speed_y = speed_y * -1
+                elif (y - 10) >= height:
+                    speed_y = speed_y * -1
+                else:
+                    x += speed_x
+                    y += speed_y
+                    scrn.fill((0, 0, 0))
+                pygame.draw.circle(scrn, ('white'), (x, y), 10)
+
+            screen.blit(scrn, (0, 0))
+        pygame.display.flip()
     pygame.quit()
--- popokatepetL.py
+++ popokatepetL.py
@@ -1,4 +1,34 @@
-g = [[0] * 5 for _ in range(6)]
-g[0][3] = 34
-for x in g:
-    print(x)
+import pygame
+
+
+size = width, height = 501, 501
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+v = 0.75
+run = True
+go = False
+x, y = 251, 251
+x_new, y_new = 251, 251
+color = pygame.Color('red')
+while run:
+    screen.fill(('black'))
+    pygame.draw.circle(screen, color, (int(x), int(y)), 20)  # рисуем наш круг
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            run = False
+
+        if e.type == pygame.MOUSEBUTTONUP and e.button == 1:  # обрабатываем событие мыши и получаем координаты
+            x_new, y_new = pygame.mouse.get_pos()
+
+    if x > x_new:  # запускаем сравние координат центра круга и расположения курсора
+        x -= v
+    elif x < x_new:
+        x += v
+    if y > y_new:
+        y -= v
+    elif y < y_new:
+        y += v
+
+    pygame.display.flip()
+    clock.tick(170)  # устанавливаем задержку в 170 кадров в секунду
+pygame.quit()
